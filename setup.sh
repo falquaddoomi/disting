6d0e1598386399c20e86c1cf97fc4aa29f5865a7
Faisal <falquaddoomi@ucla.edu>
2015-11-19 17:40:28 -0500

    Inlined virtualenv into project, added setup and launch scripts to ease deployment

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "*** DISTING requirements installer v0.1"
+
+echo -e "\n=== STEP 1. Install system packages via apt-get"
+
+sudo apt-get install rabbitmq-server
+sudo apt-get install maxima
+sudo apt-get install python python-pip
+sudo apt-get install gfortran libopenblas-dev liblapack-dev
+sudo apt-get install screen
+
+sudo pip install virtualenv
+
+echo -e "\n=== STEP 2. Create project-local virtualenv and install python packages into it"
+
+virtualenv .venv
+.venv/bin/pip install -r requirements.txt
+
+echo -e "\nComplete!"
+echo "Start the project via the launch.sh script, which will start the interface, workers, and MAXIMA daemon"
