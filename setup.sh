74330c83c7aebcab28185e9f1d97d29bbfe24fe8
Faisal <falquaddoomi@ucla.edu>
2018-03-09 12:37:48 +0100

    Updated setup.sh to install a working version of maxima and ensure that it won't be accidentally upgraded

--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@ echo "*** DISTING requirements installer v0.1"
 
 echo -e "\n=== STEP 1. Install system packages via apt-get"
 
-sudo apt-get install rabbitmq-server
-sudo apt-get install maxima
-sudo apt-get install python python-dev python-pip
-sudo apt-get install gfortran libopenblas-dev liblapack-dev
-sudo apt-get install screen
+# note that maxima 5.32.1-1 is only in trusty;
+# the below line will add that to /etc/apt/sources.list
+sudo add-apt-repository -u "deb http://ch.archive.ubuntu.com/ubuntu/ trusty universe"
+sudo apt-get install rabbitmq-server maxima=5.32.1-1 python python-dev python-pip gfortran libopenblas-dev liblapack-dev screen
+
+# note that we *never* want to upgrade maxima due to them introducing breaking changes in minor version changes
+sudo apt-mark hold maxima
 
 sudo pip install virtualenv
 
