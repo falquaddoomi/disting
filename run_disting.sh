637fda9940d2f8e2ee1cfba9481115bd60cd3907
falquaddoomi <falquaddoomi@ucla.edu>
2019-03-05 19:03:01 +0000

    Added port mappings, fixed a typo in the env file reporting message

--- run_disting.sh
+++ run_disting.sh
@@ -3,9 +3,9 @@
 if [ ! -f .env ]; then
   echo "ERROR: you need to create a .env file containing at least the following:"
   echo "ADMIN_USER=yourname"
-  echo "ADMIN_USER=yourpass"
+  echo "ADMIN_PASS=yourpass"
   exit -1
 fi
 
 docker build -t disting .
-docker run -d --rm --security-opt seccomp=unconfined -v $(pwd):/app --env-file=.env disting
+docker run --security-opt seccomp=unconfined -p 5580:5580 -p 9001:9001 -v $(pwd):/app --env-file=.env disting
