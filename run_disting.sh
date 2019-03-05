080c54839ad8262406242f2b1040cb642a0b3f59
faisala <alquaddoomi@nexus.ethz.ch>
2019-03-05 20:24:10 +0100

    DISTING now daemonizes itself by default, prints out some info to the end-user on how to control it

--- run_disting.sh
+++ run_disting.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+function postmsg() {
+  echo ""
+  echo "---"
+  echo "Success! DISTING is now running in the background at http://localhost:5580."
+  echo ""
+  echo "Notes:"
+  echo "- Use docker ps to see the list of docker processes that are running."
+  echo "- Use docker stop <container name> to stop it, and docker rm <container name> to clean it up afterward."
+  echo "- You can also stop and restart subprocesses via supervisord's admin page at http://localhost:9001"
+  echo "  (the username and password are the ones from your .env file)"
+  echo "---"
+}
+
 if [ ! -f .env ]; then
   echo "ERROR: you need to create a .env file containing at least the following:"
   echo "ADMIN_USER=yourname"
@@ -8,4 +21,5 @@ if [ ! -f .env ]; then
 fi
 
 docker build -t disting .
-docker run --security-opt seccomp=unconfined -p 5580:5580 -p 9001:9001 -v $(pwd):/app --env-file=.env disting
+docker run -d --security-opt seccomp=unconfined -p 5580:5580 -p 9001:9001 -v $(pwd):/app --env-file=.env disting \
+  && postmsg
